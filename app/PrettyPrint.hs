564bbfd54baef38aed5153a813fb8163488bdae4
Leon Vatthauer <leon.vatthauer@fau.de>
2023-01-16 18:53:04 +0100

    added ppExpr (ECase)

--- app/PrettyPrint.hs
+++ app/PrettyPrint.hs
@@ -11,30 +11,33 @@ class ISeq t where
     iIndent :: t -> t
     iDisplay :: t -> String
 
-pprExpr :: (ISeq b) => CoreExpr -> b
+pprExpr :: (ISeq b) => (Show b) => CoreExpr -> b
 pprExpr (ENum n) = iStr (show n)
 pprExpr (EVar v) = iStr v
-pprExpr (EAp e1 e2) = (pprExpr e1) `iAppend` (iStr " ") `iAppend` (pprExpr e2)
+pprExpr (EAp e1 e2) = pprExpr e1 `iAppend` iStr " " `iAppend` pprExpr e2
 pprExpr (ELet isrec defns expr) 
     = iConcat [ iStr keyword, iNewline,
                 iStr " ", iIndent (pprDefns defns), iNewline,
                 iStr "in ", pprExpr expr ]
     where keyword = if isrec then "letrec" else "let"
--- TODO pprExpr (ECase )
---      pprExpr (ELam )
+pprExpr (ECase expr alters) = iConcat [ iStr "case ", pprExpr expr, iStr " of", iNewline, iConcat (map pprAlter alters) ]
+    where pprAlter :: (ISeq b) => (Show b) => Alter Name -> b
+          pprAlter (id, args, expr) = iConcat [ iStr "<", iStr (show id), iStr "> ", iInterleave (iStr " ") (map iStr args), 
+                                                iStr " -> ", pprExpr expr ]
+-- TODO pprExpr (ELam )
 --      pprAexpr
 --      pprProgram
 -- Page 15!!
 
-pprDefns :: (ISeq b) => [(Name, CoreExpr)] -> b
+pprDefns :: (ISeq b) => (Show b) => [(Name, CoreExpr)] -> b
 pprDefns defns = iInterleave sep (map pprDefn defns)
                  where sep = iConcat [ iStr ";", iNewline ]
 
-pprDefn :: (ISeq b) => (Name, CoreExpr) -> b
+pprDefn :: (ISeq b) => (Show b) => (Name, CoreExpr) -> b
 pprDefn (name, expr) = iConcat [ iStr name, iStr " = ", iIndent (pprExpr expr)]
 
-iConcat :: (ISeq b) => [b] -> b
-iConcat bs = foldl iAppend iNil bs
+iConcat :: (ISeq b) => (Show b) => [b] -> b
+iConcat = foldl iAppend iNil
 
-iInterleave :: (ISeq b) => b -> [b] -> b
-iInterleave x xs = iConcat $ intersperse x xs
+iInterleave :: (ISeq b) => (Show b) => b -> [b] -> b
+iInterleave x xs = iConcat (intersperse x xs)
