8297238c5985313b673dc9553fc4f1bd41955e98
Leon Vatthauer <leon.vatthauer@fau.de>
2023-01-19 11:49:03 +0100

    Pretty printer is now usable

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
-import Syntax
 import PrettyPrint
+import Syntax
 
 atomExpr :: CoreExpr
 atomExpr = EVar "test"
@@ -9,9 +9,17 @@ atomExpr = EVar "test"
 nonAtomExpr :: CoreExpr
 nonAtomExpr = EAp (ENum 5) (ENum 10)
 
+lamExpr :: CoreExpr
+lamExpr = ELam ["x", "y", "z"] (EAp (EVar "x") (EVar "y"))
+
+letExpr :: CoreExpr
+letExpr = ELet recursive [("f\n", EAp (EVar "x") (EVar "y")), ("double", EAp (EVar "x") (EVar "x"))] (EAp (EVar "f") (EVar "z"))
+
 main :: IO ()
 main = do
-    print (EVar "test" :: CoreExpr)
-    print (isAtomicExpr nonAtomExpr)
-    print (isAtomicExpr atomExpr)
-    print preludeDefs
+  print (EVar "test" :: CoreExpr)
+  print (isAtomicExpr nonAtomExpr)
+  print (isAtomicExpr atomExpr)
+  print (iDisplay (pprExpr nonAtomExpr :: ISeqRep))
+  putStrLn (iDisplay (pprExpr lamExpr :: ISeqRep))
+  putStrLn (iDisplay (pprExpr letExpr :: ISeqRep))
